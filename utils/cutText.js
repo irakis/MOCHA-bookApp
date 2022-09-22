6afb5c5c36644cda2f78e9d148c5dd161da5eb35
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-09-22 13:45:02 +0200

    bitwise to logic operator

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,6 +1,6 @@
 module.exports = (content, maxLength) => {
   if (typeof content !== 'string') throw 'Error';
-  if (typeof maxLength !== 'number' | maxLength <= 0 | maxLength == Infinity) throw 'Error';
+  if (typeof maxLength !== 'number' || maxLength <= 0 || maxLength == Infinity) throw 'Error';
   if (!content.length) throw 'Error';
   if (content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
